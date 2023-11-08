86456733f71a25bc1e851e44601667685eb0f1e1
ruanlsdn <ruanlsdn@gmail.com>
2023-11-08 20:13:52 -0300

    feat: add botao no header direito da tela de produtos

--- src/routes/BottomTabNavigation.tsx
+++ src/routes/BottomTabNavigation.tsx
@@ -1,16 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { HistoryScreen, OrderScreen, ProductsScreen } from '../pages';
+import { View, Text } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { HistoryScreen, OrderScreen, ProductsScreen } from "../pages";
+import { ButtonHeaderRight } from "../components";
+import { Plus } from "@tamagui/lucide-icons";
 
 const BottomTab = createBottomTabNavigator();
 
 export default function BottomTabNavigation() {
+  const handleProductsButton = () => {
+    console.log(1);
+  };
+
   return (
-        <BottomTab.Navigator>
-            <BottomTab.Screen name='order' component={OrderScreen}/>
-            <BottomTab.Screen name='products' component={ProductsScreen}/>
-            <BottomTab.Screen name='history' component={HistoryScreen}/>
-        </BottomTab.Navigator>
-    )
+    <BottomTab.Navigator>
+      <BottomTab.Screen name="order" component={OrderScreen} />
+      <BottomTab.Screen
+        options={{
+          headerTitle: "Produtos",
+          headerRight: () => (
+            <ButtonHeaderRight
+              icon={<Plus size="$2"/>}
+              handleFunction={handleProductsButton}
+            />
+          ),
+        }}
+        name="products"
+        component={ProductsScreen}
+      />
+      <BottomTab.Screen name="history" component={HistoryScreen} />
+    </BottomTab.Navigator>
+  );
 }
