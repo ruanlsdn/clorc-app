89d9d1478c71af687a47f2eb3f4b58d136cf2e2f
ruanlsdn <ruanlsdn@gmail.com>
2023-11-07 22:39:16 -0300

    chore: configurando o tamagui no projeto

--- App.tsx
+++ App.tsx
@@ -1,13 +1,28 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from 'expo-font';
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { TamaguiProvider } from "tamagui";
 import NativeStackNavigation from "./src/routes/NativeStackNavigation";
+import tamaguiConfig from "./tamagui.config";
+import { useEffect } from "react";
 
 export default function App() {
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+  
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <NativeStackNavigation />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <TamaguiProvider config={tamaguiConfig}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <NativeStackNavigation />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </TamaguiProvider>
   );
 }
