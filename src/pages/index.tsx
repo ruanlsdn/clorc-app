86b954534321396f95c137c4556aea735a768a79
ruanlsdn <ruanlsdn@gmail.com>
2023-11-07 21:08:23 -0300

    feat: add primeiras paginas do app

---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+export { default as OrderScreen } from "./OrderScreen";
+export { default as ProductsScreen } from "./ProductsScreen";
+export { default as HistoryScreen } from "./HistoryScreen";
+export { default as LoginScreen } from "./LoginScreen";
