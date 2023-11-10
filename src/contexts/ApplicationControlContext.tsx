d8c243c5c32c4ac8dabe817f22f963f0b90a9c45
ruanlsdn <ruanlsdn@gmail.com>
2023-11-09 23:37:22 -0300

    refactor: adaptando contexto para receber o ActionEnum

--- src/contexts/ApplicationControlContext.tsx
+++ src/contexts/ApplicationControlContext.tsx
@@ -1,6 +1,9 @@
 import React, { createContext, useContext, useState } from "react";
+import { ActionEnum } from "../enums/ActionEnum";
 
 type ApplicationControlContextProps = {
+  action: ActionEnum;
+  setAction: React.Dispatch<React.SetStateAction<ActionEnum>>;
   isAdaptedDialogOpen: boolean;
   setIsAdaptedDialogOpen: React.Dispatch<React.SetStateAction<boolean>>;
   isAlertOpen: boolean;
@@ -16,12 +19,15 @@ type ChildrenProps = {
 };
 
 export const ApplicationControlProvider = ({ children }: ChildrenProps) => {
+  const [action, setAction] = useState(ActionEnum.NONE);
   const [isAdaptedDialogOpen, setIsAdaptedDialogOpen] = useState(false);
   const [isAlertOpen, setIsAlertOpen] = useState(false);
 
   return (
     <ApplicationControlContext.Provider
       value={{
+        action,
+        setAction,
         isAdaptedDialogOpen,
         setIsAdaptedDialogOpen,
         isAlertOpen,
