ad939a97b85c195e7f6f5ec562ec91780003a413
ruanlsdn <ruanlsdn@gmail.com>
2023-11-10 02:13:36 -0300

    feat: add abertura de modal e alert no acionamento dos botoes de editar, excluir e cadastrar

--- src/pages/ProductsScreen/index.tsx
+++ src/pages/ProductsScreen/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
-import { ProductsList, Searchbar } from "../../components";
+import {
+  AdaptedDialog,
+  Alert,
+  ProductsList,
+  Searchbar,
+} from "../../components";
 import { iProduct } from "../../interfaces";
+import { useApplicationControlContext } from "../../contexts";
+import { ActionEnum } from "../../enums/ActionEnum";
 
 const dummy: iProduct[] = [
   {
@@ -8,40 +15,78 @@ const dummy: iProduct[] = [
     price: 3.0,
   },
   {
-    title: "Item 1",
+    title: "Item 2",
     price: 3.0,
   },
   {
-    title: "Item 1",
+    title: "Item 3",
     price: 3.0,
   },
   {
-    title: "Item 1",
+    title: "Item 4",
     price: 3.0,
   },
   {
-    title: "Item 1",
+    title: "Item 5",
     price: 3.0,
   },
   {
-    title: "Item 1",
+    title: "Item 6",
     price: 3.0,
   },
   {
-    title: "Item 1",
+    title: "Item 7",
     price: 3.0,
   },
   {
-    title: "Item 1",
+    title: "Item 8",
     price: 3.0,
   },
 ];
 
 export default function ProductsScreen() {
+  const {
+    action,
+    isAdaptedDialogOpen,
+    setIsAdaptedDialogOpen,
+    isAlertOpen,
+    setIsAlertOpen,
+  } = useApplicationControlContext();
+
+  const showAdaptedDialog = (action: ActionEnum) => {
+    switch (action) {
+      case ActionEnum.CREATE:
+        return (
+          <AdaptedDialog
+            title="Cadastrar"
+            description="Preencha os campos abaixo e confirme para salvar o produto:"
+            isOpen={isAdaptedDialogOpen}
+            setIsOpen={setIsAdaptedDialogOpen}
+          />
+        );
+      case ActionEnum.UPDATE:
+        return (
+          <AdaptedDialog
+            title="Editar"
+            description="Atualize os campos abaixo e confirme para salvar suas alterações:"
+            isOpen={isAdaptedDialogOpen}
+            setIsOpen={setIsAdaptedDialogOpen}
+          />
+        );
+    }
+  };
+
   return (
     <>
       <Searchbar />
       <ProductsList list={dummy} />
+      <Alert
+        title="Excluir"
+        description="Tem certeza que deseja excluir o produto?"
+        isOpen={isAlertOpen}
+        setIsOpen={setIsAlertOpen}
+        />
+        {showAdaptedDialog(action)}
     </>
   );
 }
