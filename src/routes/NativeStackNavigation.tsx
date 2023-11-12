c3e825e6840f8c003ab6168bda25aa989fc53f4b
ruanlsdn <ruanlsdn@gmail.com>
2023-11-11 22:14:10 -0300

    chore: definindo header da stackNavigation como invisivel

--- src/routes/NativeStackNavigation.tsx
+++ src/routes/NativeStackNavigation.tsx
@@ -1,14 +1,14 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { LoginScreen } from '../pages';
-import BottomTabNavigation from './BottomTabNavigation';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { LoginScreen } from "../pages";
+import BottomTabNavigation from "./BottomTabNavigation";
 
 const NativeStack = createNativeStackNavigator();
 
 export default function NativeStackNavigation() {
   return (
-    <NativeStack.Navigator>
-        {/* <NativeStack.Screen options={{headerShown: false}} name='login' component={LoginScreen}/> */}
-        <NativeStack.Screen options={{headerShown: false}} name='menu' component={BottomTabNavigation}/>
+    <NativeStack.Navigator screenOptions={{ headerShown: false, }}>
+      {/* <NativeStack.Screen options={{headerShown: false}} name='login' component={LoginScreen}/> */}
+      <NativeStack.Screen name="menu" component={BottomTabNavigation} />
     </NativeStack.Navigator>
-  )
+  );
 }
