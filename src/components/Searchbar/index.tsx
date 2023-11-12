aeaa5d52ae86dc0fc3d1517bf5dfb746ee893e82
ruanlsdn <ruanlsdn@gmail.com>
2023-11-11 22:15:38 -0300

    style: add cores ao componente de searchbar

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -5,9 +5,33 @@ import { Search } from "@tamagui/lucide-icons";
 
 export default function Searchbar() {
   return (
-    <XStack margin="$3" height="$5" alignItems="center" justifyContent="center">
-      <Input placeholder="Pesquisar..." fontSize="$5" width="90%" />
-      <Button icon={Search} bordered />
+    <XStack
+      padding="$4"
+      alignItems="center"
+      justifyContent="center"
+      bc="#202123"
+    >
+      <Input
+        placeholder="Pesquisar..."
+        fontSize="$5"
+        width="90%"
+        bc="#565869"
+        borderColor="#565869"
+        borderTopRightRadius="$0"
+        borderBottomRightRadius="$0"
+        color="#ffffff"
+      />
+      <Button
+        bc="#343541"
+        borderTopLeftRadius="$0"
+        borderBottomLeftRadius="$0"
+        icon={<Search color="#19C37D" size="$1"/>}
+        pressStyle={{
+          opacity: 0.5,
+          borderColor: "#343541",
+          backgroundColor: "#343541",
+        }}
+      />
     </XStack>
   );
 }
