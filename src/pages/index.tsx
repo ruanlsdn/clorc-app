a748742c7779e75e41248cdad00a05ed3ff27f33
ruanlsdn <ruanlsdn@gmail.com>
2023-11-14 20:21:31 -0300

    feat: criando a tela de carrinho

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,3 +2,4 @@ export { default as OrderScreen } from "./OrderScreen";
 export { default as ProductsScreen } from "./ProductsScreen";
 export { default as HistoryScreen } from "./HistoryScreen";
 export { default as LoginScreen } from "./LoginScreen";
+export { default as CartScreen } from "./CartScreen";
