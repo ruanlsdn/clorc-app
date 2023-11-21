3d8902f9c3dcd7b3ed25d2496073ba06d6441d1e
ruanlsdn <ruanlsdn@gmail.com>
2023-11-21 17:31:56 -0300

    feat: add funcionalidade de cadastrar produto

--- src/components/UpsertProduct/index.tsx
+++ src/components/UpsertProduct/index.tsx
@@ -1,8 +1,41 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
 import { Button, Input, Label, XStack, YStack } from "tamagui";
+import { useAxios } from "../../hooks";
+import { iProduct } from "../../interfaces";
+import { useApplicationControlContext } from "../../contexts";
+import { axiosProductService } from "../../services";
 
-export default function UpsertProduct() {
+type props = {
+  isUpdate: boolean;
+};
+
+export default function UpsertProduct({ isUpdate }: props) {
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [stock, setStock] = useState("");
+  const { data, status, error, loading, fetchData } = useAxios<iProduct>();
+  const { setIsCreateProductDialogOpen } = useApplicationControlContext();
+
+  const handleSubmit = async () => {
+    await fetchData(
+      {
+        axiosInstance: axiosProductService,
+        method: "post",
+        url: "",
+      },
+      {
+        description,
+        price: Number(price),
+        userId: "439b0584-35b7-4486-b8a1-1165c19a26e1",
+      }
+    );
+
+    if (status === 201) {
+      setIsCreateProductDialogOpen(false);
+    }
+  };
+
   return (
     <>
       <YStack space>
@@ -10,21 +43,29 @@ export default function UpsertProduct() {
           <Label disabled color="#D9D9E3">
             Descrição:
           </Label>
-          <Input bc="#D9D9E3" />
+          <Input onChangeText={(text) => setDescription(text)} bc="#D9D9E3" />
         </YStack>
         <XStack space>
           <YStack flex={1}>
             <Label disabled color="#D9D9E3">
               Valor unitário:
             </Label>
-            <Input keyboardType="numeric" bc="#D9D9E3" />
+            <Input
+              onChangeText={(text) => setPrice(text)}
+              keyboardType="numeric"
+              bc="#D9D9E3"
+            />
           </YStack>
-          {false && (
+          {isUpdate && (
             <YStack flex={1}>
               <Label disabled color="#D9D9E3">
                 Estoque:
               </Label>
-              <Input keyboardType="numeric" bc="#D9D9E3" />
+              <Input
+                onChangeText={(text) => setStock(text)}
+                keyboardType="numeric"
+                bc="#D9D9E3"
+              />
             </YStack>
           )}
         </XStack>
@@ -38,6 +79,7 @@ export default function UpsertProduct() {
           borderColor: "#19C37D",
           backgroundColor: "#19C37D",
         }}
+        onPress={handleSubmit}
       >
         Confirmar
       </Button>
