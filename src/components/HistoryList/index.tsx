2bb35e9a50ca766c6e2d212f00d29b3bf301b8aa
ruanlsdn <ruanlsdn@gmail.com>
2024-11-29 20:52:50 -0300

    refactor: refatoracao para integrar com funcionalidade de filtro da searchbar

--- src/components/HistoryList/index.tsx
+++ src/components/HistoryList/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { ScrollView } from "tamagui";
-import { iHistoryOrder } from "../../interfaces";
-import HistoryListItem from "../HistoryListItem";
+import React from 'react';
+import { ScrollView, View, Text } from 'tamagui';
+import { iHistoryOrder } from '../../interfaces';
+import HistoryListItem from '../HistoryListItem';
 
 type props = {
   list: iHistoryOrder[];
@@ -9,10 +9,19 @@ type props = {
 
 export default function HistoryList({ list }: props) {
   return (
-    <ScrollView flex={1} bc="#202123">
-      {list?.map((item, idx) => (
-        <HistoryListItem key={idx} item={item} />
-      ))}
-    </ScrollView>
+    <>
+      {list.length > 0 && (
+        <ScrollView bc='#202123'>
+          {list?.map((item, idx) => (
+            <HistoryListItem key={idx} item={item} />
+          ))}
+        </ScrollView>
+      )}
+      {list.length === 0 && (
+        <View flex={1} bc='#202123' alignItems='center' justifyContent='center'>
+          <Text color={'whitesmoke'}>Nenhum registro encontrado.</Text>
+        </View>
+      )}
+    </>
   );
 }
--- src/pages/HistoryScreen/index.tsx
+++ src/pages/HistoryScreen/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HistoryList, Searchbar } from '../../components';
 import { useDataControlContext } from '../../contexts';
 
 export default function HistoryScreen() {
   const { cards } = useDataControlContext();
+  const [filteredCards, setFilteredCards] = useState(cards);
 
   return (
     <>
-      <Searchbar />
-      <HistoryList list={cards} />
+      <Searchbar
+        placeholder='Pesquisar cliente...'
+        list={cards}
+        searchParameter='clientName'
+        onFilterUpdate={setFilteredCards}
+      />
+      <HistoryList list={filteredCards} />
     </>
   );
 }
