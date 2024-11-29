0dd31009ba78a10d63fad4c2ab00a28f0412da65
ruanlsdn <ruanlsdn@gmail.com>
2024-11-29 20:31:49 -0300

    refactor: refatoracao para integrar com funcionalidade de filtro da searchbar

--- src/components/OrderList/index.tsx
+++ src/components/OrderList/index.tsx
@@ -1,8 +1,7 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { iProduct } from "../../interfaces";
-import { ScrollView } from "tamagui";
-import OrderListItem from "../OrderListItem";
+import React from 'react';
+import { ScrollView, View, Text } from 'tamagui';
+import { iProduct } from '../../interfaces';
+import OrderListItem from '../OrderListItem';
 
 type props = {
   list: iProduct[];
@@ -10,10 +9,19 @@ type props = {
 
 export default function OrderList({ list }: props) {
   return (
-    <ScrollView bc="#202123">
-      {list.map((item, idx) => (
-        <OrderListItem key={idx} item={item} />
-      ))}
-    </ScrollView>
+    <>
+      {list.length > 0 && (
+        <ScrollView bc='#202123'>
+          {list.map((item, idx) => (
+            <OrderListItem key={idx} item={item} />
+          ))}
+        </ScrollView>
+      )}
+      {list.length === 0 && (
+        <View flex={1} bc='#202123' alignItems='center' justifyContent='center'>
+          <Text color={'whitesmoke'}>Nenhum registro encontrado.</Text>
+        </View>
+      )}
+    </>
   );
 }
--- src/pages/OrderScreen/index.tsx
+++ src/pages/OrderScreen/index.tsx
@@ -1,23 +1,32 @@
-import React from "react";
-import { Alert, IncreaseAmount, OrderList, Searchbar } from "../../components";
-import {
-  useApplicationControlContext,
-  useDataControlContext,
-} from "../../contexts";
+import React, { useEffect, useState } from 'react';
+import { Alert, IncreaseAmount, OrderList, Searchbar } from '../../components';
+import { useApplicationControlContext, useDataControlContext } from '../../contexts';
 
 export default function OrderScreen() {
   const { products } = useDataControlContext();
   const { isIncreaseAmountAlertOpen, setIsIncreaseAmountAlertOpen } = useApplicationControlContext();
+  const [filteredProducts, setFilteredProducts] = useState(products);
+
+  useEffect(() => {
+    if (products.length > 0) {
+      setFilteredProducts(products);
+    }
+  }, [products])
 
   return (
     <>
-      <Searchbar />
-      <OrderList list={products} />
+      <Searchbar
+        placeholder='Pesquisar produto...'
+        list={products}
+        searchParameter='description'
+        onFilterUpdate={setFilteredProducts}
+      />
+      <OrderList list={filteredProducts} />
       <Alert
         isOpen={isIncreaseAmountAlertOpen}
         setIsOpen={setIsIncreaseAmountAlertOpen}
-        title="Inserir no carrinho"
-        description="Informe a quantidade desejada e confirme para inserir no carrinho: "
+        title='Inserir no carrinho'
+        description='Informe a quantidade desejada e confirme para inserir no carrinho: '
         children={<IncreaseAmount />}
       />
     </>
