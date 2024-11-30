653dd8645e91fd4d3250e916e7bda1b0ee06b2e6
ruanlsdn <ruanlsdn@gmail.com>
2024-11-29 21:04:42 -0300

    fix: ajuste para atualizar a lista filtrada sempre que o contexto atualziar

--- src/pages/HistoryScreen/index.tsx
+++ src/pages/HistoryScreen/index.tsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { HistoryList, Searchbar } from '../../components';
 import { useDataControlContext } from '../../contexts';
 
 export default function HistoryScreen() {
   const { cards } = useDataControlContext();
   const [filteredCards, setFilteredCards] = useState(cards);
+
+  useEffect(() => {
+    if (cards.length > 0) {
+      setFilteredCards(cards);
+    }
+  }, [cards])
 
   return (
     <>
