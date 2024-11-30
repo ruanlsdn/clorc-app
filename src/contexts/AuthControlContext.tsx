d85f3eb166e8ceec59a3b8c903b279f62b7b2599
ruanlsdn <ruanlsdn@gmail.com>
2024-11-30 20:05:15 -0300

    refactor: refatoracao para remover o uso do hook useAxios

--- src/contexts/AuthControlContext.tsx
+++ src/contexts/AuthControlContext.tsx
@@ -1,9 +1,11 @@
-import { useNavigation } from '@react-navigation/native';
-import { createContext, useContext, useState } from 'react';
-import { useAxios } from '../hooks';
-import { axiosAuthService } from '../services';
-import { iUser } from '../interfaces';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import { UserX2, XCircle } from '@tamagui/lucide-icons';
+import { useToastController } from '@tamagui/toast';
+import { AxiosError, AxiosResponse } from 'axios';
+import { createContext, useContext, useState } from 'react';
+import { iUser } from '../interfaces';
+import { axiosAuthService } from '../services';
 
 type AuthControlContextProps = {
   user: iUser;
@@ -18,41 +20,49 @@ type props = {
 export const AuthControlContext = createContext<AuthControlContextProps>(null!);
 
 export const AuthControlProvider = ({ children }: props) => {
-  const { fetchData } = useAxios<iUser, iUser>();
+  const toast = useToastController();
   const navigation = useNavigation();
 
   const [user, setUser] = useState<iUser>(undefined!);
 
-  function login(username: string, password: string) {
-    fetchData(
-      {
-        axiosInstance: axiosAuthService,
-        method: 'post',
-        url: '/login',
-      },
-      { login: username, password: password },
-    )
-      .then(async (user) => {
-        if (user !== undefined && user.active) {
-          await AsyncStorage.setItem('@isLoggedIn', 'true');
-          await AsyncStorage.setItem('@username', username);
-          await AsyncStorage.setItem('@password', password);
+  async function login(username: string, password: string) {
+    try {
+      const response: AxiosResponse<iUser> = await axiosAuthService.post(
+        '/login',
+        { login: username, password: password, cidade: true },
+        { timeout: 5 * 1000 },
+      );
+      if (response.data !== undefined && response.data.active) {
+        await AsyncStorage.setItem('@isLoggedIn', 'true');
+        await AsyncStorage.setItem('@username', username);
+        await AsyncStorage.setItem('@password', password);
 
-          setUser(user);
-          navigation.navigate('menu' as never);
-        } else {
-          console.log('Usuário inativo!')
-        }
-      })
-      .catch((error) => {
-        console.log('error ', error);
+        setUser(user);
+        navigation.navigate('menu' as never);
+      } else {
+        toast.show('Usuário inativo!', {
+          message: 'Contate o Administrador do sistema.',
+          viewportName: 'main',
+          customData: { icon: <UserX2 size={25} /> },
+        });
+      }
+    } catch (error) {
+      const err = error as AxiosError;
+      const status = err.response?.status;
+      const title = status ? `${status} - Ocorreu um erro!` : 'Ocorreu um erro!';
+      const message = status && status === 401 ? 'Dados incorretos ou usuário não cadastrado.' : 'Tente novamente em alguns instantes.';
+      toast.show(title, {
+        message: message,
+        viewportName: 'main',
+        customData: { icon: <XCircle size={25} /> },
       });
+    }
   }
 
   async function logout() {
     await AsyncStorage.setItem('@isLoggedIn', 'false');
     await AsyncStorage.multiRemove(['@username', '@password']);
-    
+
     setUser(undefined!);
     navigation.navigate('login' as never);
   }
