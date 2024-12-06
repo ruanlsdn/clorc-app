bf876ef1bba9021259e2cde4c0083c17a4d7e20a
ruanlsdn <ruanlsdn@gmail.com>
2024-12-05 21:08:07 -0300

    style: alterando tamanho da fonte do html q sera usado na imagem

--- src/helpers/reports/generate-order-report-body-image-html.ts
+++ src/helpers/reports/generate-order-report-body-image-html.ts
@@ -7,7 +7,7 @@ export function generateBodyImageHtml(username: string, clientName: string, addr
         ${clientName || address || deliveryDate ? generateClientInfo(clientName, address, deliveryDate) : ''}
         ${generateProductsTable(products)}
         ${generateSummary(products)}
-        ${shouldGenerateFooter ? generateFooter() : `<b style="font-size: 45px">━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>`}
+        ${shouldGenerateFooter ? generateFooter() : `<b style="font-size: 42px">━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>`}
       `;
 
   return body;
@@ -16,8 +16,8 @@ export function generateBodyImageHtml(username: string, clientName: string, addr
 function generateHeader(username: string) {
   return `
         <div style="display: flex; flex-direction: column; align-items: center; justify-content: center;" >
-            <b style="font-size: 55px">${username.toUpperCase()}</b>
-            <b style="font-size: 45px">━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>
+            <b style="font-size: 42px">${username.toUpperCase()}</b>
+            <b style="font-size: 42px">━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>
         </div>
     `;
 }
@@ -27,22 +27,22 @@ function generateClientInfo(clientName: string, address: string, deliveryDate: D
         <div style="display: flex; flex-direction: column;" >
             ${
               deliveryDate
-                ? `<div style="display: flex; flex-direction: row; align-items: center; justify-content: space-between;" > <b style="font-size: 50px">AGENDADO PARA: </b> <b style="font-size: 50px">${deliveryDate.toLocaleDateString(
+                ? `<div style="display: flex; flex-direction: row; align-items: center; justify-content: space-between;" > <b style="font-size: 42px">AGENDADO PARA: </b> <b style="font-size: 42px">${deliveryDate.toLocaleDateString(
                     'pt-br',
                   )}</b> </div>`
                 : ''
             }
             ${
               clientName
-                ? `<div style="display: flex; flex-direction: row; align-items: center; justify-content: space-between;" > <b style="font-size: 50px">CLIENTE: </b> <b style="font-size: 50px">${clientName.toUpperCase()}</b> </div>`
+                ? `<div style="display: flex; flex-direction: row; align-items: center; justify-content: space-between;" > <b style="font-size: 42px">CLIENTE: </b> <b style="font-size: 42px">${clientName.toUpperCase()}</b> </div>`
                 : ''
             }
             ${
               address
-                ? `<div style="display: flex; flex-direction: row; align-items: center; justify-content: space-between;" > <b style="font-size: 50px">ENDEREÇO: </b> <b style="font-size: 50px">${address.toUpperCase()}</b> </div>`
+                ? `<div style="display: flex; flex-direction: row; align-items: center; justify-content: space-between;" > <b style="font-size: 42px">ENDEREÇO: </b> <b style="font-size: 42px">${address.toUpperCase()}</b> </div>`
                 : ''
             }
-            <b style="font-size: 45px">━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>
+            <b style="font-size: 42px">━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>
         </div>
     `;
 }
@@ -50,21 +50,21 @@ function generateClientInfo(clientName: string, address: string, deliveryDate: D
 function generateProductsTable(products: iProduct[]) {
   let header = `
           <div style="display: flex; flex-direction: column; align-items: center; justify-content: center;" >
-            <b style="font-size: 55px">${'PEDIDO'}</b>
-            <b style="font-size: 45px">━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>
+            <b style="font-size: 42px">${'PEDIDO'}</b>
+            <b style="font-size: 42px">━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>
           </div>
           <div style="display: flex; flex-direction: row; align-items: center; width: 100%" >
             <div style="width: 10%; display: flex; align-items: center; justify-content: flex-start;">
-                <b style="font-size: 55px">QTD</b>
+                <b style="font-size: 42px">QTD</b>
             </div>
             <div style="width: 45%; display: flex; align-items: center; justify-content: center;">
-                <b style="font-size: 55px">DESCRIÇÃO</b>
+                <b style="font-size: 42px">DESCRIÇÃO</b>
             </div>
             <div style="width: 20%;  display: flex; align-items: center; justify-content: center;">
-                <b style="font-size: 55px">P.UNIT</b>
+                <b style="font-size: 42px">P.UNIT</b>
             </div>
             <div style="width: 25%;  display: flex; align-items: center; justify-content: flex-end;">
-                <b style="font-size: 55px">SUBTOTAL</b>
+                <b style="font-size: 42px">SUBTOTAL</b>
             </div>
           </div>
         `;
@@ -75,16 +75,16 @@ function generateProductsTable(products: iProduct[]) {
     productsRow += `
             <div style="display: flex; flex-direction: row; align-items: center; width: 100%" >
                 <div style=" display: flex; width: 10%; align-items: center; justify-content: flex-start;">
-                    <b style="font-size: 45px">${product.quantity}</b>
+                    <b style="font-size: 42px">${product.quantity}</b>
                 </div>
                 <div style=" display: flex; width: 45%; align-items: center; justify-content: center;">
-                    <b style="font-size: 45px">${product.description}</b>
+                    <b style="font-size: 42px">${product.description}</b>
                 </div>
                 <div style=" display: flex; width: 20%; align-items: center; justify-content: center;">
-                    <b style="font-size: 45px">R$ ${product.price?.toFixed(2)}</b>
+                    <b style="font-size: 42px">R$ ${product.price?.toFixed(2)}</b>
                 </div>
                 <div style=" display: flex; width: 25%; align-items: center; justify-content: flex-end;">
-                    <b style="font-size: 45px">R$ ${(product.quantity! * product.price!).toFixed(2)}</b>
+                    <b style="font-size: 42px">R$ ${(product.quantity! * product.price!).toFixed(2)}</b>
                 </div>
             </div>
             `;
@@ -103,18 +103,18 @@ function generateSummary(products: iProduct[]) {
   }
 
   return `
-        <b style="font-size: 45px">━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>
+        <b style="font-size: 42px">━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>
           <div style="display: flex; flex-direction: column; align-items: center; justify-content: center;" >
-            <b style="font-size: 55px">RESUMO DO PEDIDO</b>
-            <b style="font-size: 45px">━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>
+            <b style="font-size: 42px">RESUMO DO PEDIDO</b>
+            <b style="font-size: 42px">━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>
           </div>
           <div style="display: flex; flex-direction: row; align-items: center; justify-content: space-between;" >
-            <b style="font-size: 50px">QUANT. ITENS</b>
-            <b style="font-size: 50px">TOTAL</b>
+            <b style="font-size: 42px">QUANT. ITENS</b>
+            <b style="font-size: 42px">TOTAL</b>
           </div>
           <div style="display: flex; flex-direction: row; align-items: center; justify-content: space-between;" >
-            <b style="font-size: 45px">${totalItems}</b>
-            <b style="font-size: 45px">R$ ${total.toFixed(2)}</b>
+            <b style="font-size: 42px">${totalItems}</b>
+            <b style="font-size: 42px">R$ ${total.toFixed(2)}</b>
           </div>
   `;
 }
@@ -122,10 +122,10 @@ function generateSummary(products: iProduct[]) {
 function generateFooter() {
   const date = moment();
   return `
-    <b style="font-size: 45px">━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>
+    <b style="font-size: 42px">━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>
       <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; margin-top: 55px">
           <b style="font-size: 55px">***ESTE NÃO É UM TICKET FISCAL***</b>
-          <b style="font-size: 45px; text-align: center; margin-top: 55px">Ticket gerado em  ${date.toDate().toLocaleString('pt-br')}.</b>
+          <b style="font-size: 42px; text-align: center; margin-top: 55px">Ticket gerado em  ${date.toDate().toLocaleString('pt-br')}.</b>
       </div>
       `;
 }
